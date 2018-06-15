33764f6300b34672dda930290c75fab799ca6b46
Kittycraft <kittycraftgithub@gmail.com>
2018-06-15 00:33:49 -0700

    Redo info command

--- src/main/java/io/github/aquerr/eaglefactions/commands/InfoCommand.java
+++ src/main/java/io/github/aquerr/eaglefactions/commands/InfoCommand.java
@@ -1,77 +1,56 @@
 package io.github.aquerr.eaglefactions.commands;
 
-import io.github.aquerr.eaglefactions.PluginInfo;
-import io.github.aquerr.eaglefactions.PluginPermissions;
 import io.github.aquerr.eaglefactions.entities.Faction;
 import io.github.aquerr.eaglefactions.logic.FactionLogic;
-import io.github.aquerr.eaglefactions.logic.PluginMessages;
+import io.github.aquerr.eaglefactions.managers.PlayerManager;
 import io.github.aquerr.eaglefactions.managers.PowerManager;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.service.pagination.PaginationList;
-import org.spongepowered.api.service.pagination.PaginationService;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 /**
  * Created by Aquerr on 2017-08-03.
+ *
+ * Finished for now.
+ *
+ * TODO:
+ * Show relationships only if there is at least one in the specific category.
  */
 public class InfoCommand implements CommandExecutor {
+
+    private static final String dash = "-----------------------------------------------";
+
     @Override
     public CommandResult execute(CommandSource source, CommandContext context) throws CommandException {
-        Optional<String> optionalFactionName = context.<String>getOne("faction name");
+        Optional<String> optionalFactionName = context.getOne("faction name");
 
+        Faction faction;
         if (optionalFactionName.isPresent()) {
-            String rawFactionName = optionalFactionName.get();
-            Faction faction = FactionLogic.getFactionByName(rawFactionName);
-
-            if (faction == null) {
-                source.sendMessage(Text.of(PluginInfo.ErrorPrefix, TextColors.RED, PluginMessages.THERE_IS_NO_FACTION_CALLED + " ", TextColors.GOLD, rawFactionName + "!"));
-            } else {
-                if (source.hasPermission(PluginPermissions.InfoCommand) || source.hasPermission(PluginPermissions.InfoCommandSelf) || source.hasPermission(PluginPermissions.InfoCommandOthers)) {
-                    //Check permissions
-                    if ((!source.hasPermission(PluginPermissions.InfoCommand) && !source.hasPermission(PluginPermissions.InfoCommandSelf)) && (source instanceof Player && FactionLogic.getFactionByPlayerUUID(((Player) source).getUniqueId()).isPresent() && FactionLogic.getFactionByPlayerUUID(((Player) source).getUniqueId()).get().Name.equals(faction.Name))) {
-                        source.sendMessage(Text.of(PluginInfo.ErrorPrefix, TextColors.RED, PluginMessages.YOU_DONT_HAVE_PERMISSIONS_FOR_VEWING_INFO_ABOUT_YOUR_FACTION));
-                    } else if ((!source.hasPermission(PluginPermissions.InfoCommand) && !source.hasPermission(PluginPermissions.InfoCommandOthers)) && (source instanceof Player && FactionLogic.getFactionByPlayerUUID(((Player) source).getUniqueId()).isPresent() && !FactionLogic.getFactionByPlayerUUID(((Player) source).getUniqueId()).get().Name.equals(faction.Name))) {
-                        source.sendMessage(Text.of(PluginInfo.ErrorPrefix, TextColors.RED, PluginMessages.YOU_DONT_HAVE_PERMISSIONS_FOR_VEWING_INFO_ABOUT_OTHER_FACTIONS));
-                    } else {
-                        showFactionInfo(source, faction);
-                    }
-                } else {
-                    source.sendMessage(Text.of(PluginInfo.ErrorPrefix, TextColors.RED, PluginMessages.YOU_DONT_HAVE_PERMISSIONS_TO_USE_THIS_COMMAND));
-                }
-            }
-        } else if (source instanceof Player && FactionLogic.getFactionByPlayerUUID(((Player) source).getUniqueId()).isPresent()) {
-            //Check permissions
-            if (source.hasPermission(PluginPermissions.InfoCommand) || source.hasPermission(PluginPermissions.InfoCommandSelf)) {
-                showFactionInfo(source, FactionLogic.getFactionByPlayerUUID(((Player) source).getUniqueId()).get());
-            } else {
-                source.sendMessage(Text.of(PluginInfo.ErrorPrefix, TextColors.RED, PluginMessages.YOU_DONT_HAVE_PERMISSIONS_FOR_VEWING_INFO_ABOUT_YOUR_FACTION));
-            }
+            faction = FactionLogic.getFactionByName(optionalFactionName.get());
         } else {
-            source.sendMessage(Text.of(PluginInfo.ErrorPrefix, TextColors.RED, PluginMessages.WRONG_COMMAND_ARGUMENTS));
-            source.sendMessage(Text.of(TextColors.RED, PluginMessages.USAGE + " /f f <faction name>"));
+            if (source instanceof Player && FactionLogic.getFactionByPlayerUUID(((Player) source).getUniqueId()).isPresent()) {
+                faction = FactionLogic.getFactionByPlayerUUID(((Player) source).getUniqueId()).get();
+            } else {
+                faction = FactionLogic.getFactionByName("wilderness");
+            }
         }
-
+        showFactionInfo(source, faction);
         return CommandResult.success();
     }
 
     private void showFactionInfo(CommandSource source, Faction faction) {
-        List<Text> factionInfo = new ArrayList<>();
-
         Text.Builder text = Text.of("").toBuilder();
-        String dash = "-----------------------------------------------";
-        text.append(Text.of(TextColors.GOLD, dash.substring(0, 25 - faction.Name.length() / 2),
-                ".[ ", TextColors.GREEN, faction.Name, TextColors.GOLD, " ].", dash.substring(0, 25 - faction.Name.length() / 2), "\nDescription: ", TextColors.YELLOW, "Not supported \u2764\n"));
+        text.append(Text.of(TextColors.GOLD, dash.substring(0, 24 - faction.Name.length() / 2),
+                ".[ ", TextColors.GREEN, faction.Name, TextColors.GOLD, " ].", dash.substring(0, 24 - faction.Name.length() / 2), "\nDescription: ", TextColors.YELLOW, "Not supported \u2764\n"));
 
         String alliancesList = "";
         if (!faction.Alliances.isEmpty()) {
@@ -93,36 +72,25 @@ public class InfoCommand implements CommandExecutor {
                 .append(Text.of(TextColors.GOLD, "Allies: ", TextColors.GREEN, alliancesList + "\n"))
                 .append(Text.of(TextColors.GOLD, "Enemies: ", TextColors.RED, enemiesList + "\n"));
 
-//        String leaderName = "";
-//        if (!faction.Leader.equals("")) leaderName = PlayerManager.getPlayerName(UUID.fromString(faction.Leader.name)).get();
-//
-//        String membersList = "";
-//        if (!faction.Members.isEmpty()) {
-//            for (String member : faction.Members) {
-//                membersList += PlayerManager.getPlayerName(UUID.fromString(member)).get() + ", ";
-//            }
-//            membersList = membersList.substring(0, membersList.length() - 2);
-//        }
-//
-//        Text info = Text.builder()
-//                .append(Text.of(TextColors.AQUA, PluginMessages.NAME + ": ", TextColors.GOLD, faction.Name + "\n"))
-//                .append(Text.of(TextColors.AQUA, PluginMessages.TAG + ": "), faction.Tag.toBuilder().color(TextColors.GOLD).build(), Text.of("\n"))
-//                .append(Text.of(TextColors.AQUA, PluginMessages.LEADER + ": ", TextColors.GOLD, leaderName + "\n"))
-//                .append(Text.of(TextColors.AQUA, PluginMessages.OFFICERS + ": ", TextColors.GOLD, officersList + "\n"))
-//                .append(Text.of(TextColors.AQUA, PluginMessages.ALLIANCES + ": ", TextColors.BLUE, alliancesList + "\n"))
-//                .append(Text.of(TextColors.AQUA, PluginMessages.ENEMIES + ": ", TextColors.RED, enemiesList + "\n"))
-//                .append(Text.of(TextColors.AQUA, PluginMessages.MEMBERS + ": ", TextColors.GREEN, membersList + "\n"))
-//                .append(Text.of(TextColors.AQUA, PluginMessages.RECRUITS + ": ", TextColors.GREEN, recruitList + "\n"))
-//                .append(Text.of(TextColors.AQUA, PluginMessages.POWER + ": ", TextColors.GOLD, PowerManager.getFactionPower(faction) + "/" + PowerManager.getFactionMaxPower(faction) + "\n"))
-//                .append(Text.of(TextColors.AQUA, PluginMessages.CLAIMS + ": ", TextColors.GOLD, String.valueOf(faction.Claims.size()) + "/" + String.valueOf(PowerManager.getFactionPower(faction).intValue())))
-//                .build();
-
-        //TODO: Stop being lazy and finish the info -Kitty
-
-        factionInfo.add(text.build());
-
-        PaginationService paginationService = Sponge.getServiceManager().provide(PaginationService.class).get();
-        PaginationList.Builder paginationBuilder = paginationService.builder().title(Text.of(TextColors.GREEN, PluginMessages.FACTION_INFO)).contents(factionInfo);
-        paginationBuilder.sendTo(source);
+        List<Player> onlinePlayers = FactionLogic.getOnlinePlayers(faction);
+        text.append(Text.of(TextColors.GOLD, "Online Players (" + onlinePlayers.size() + "): ", TextColors.YELLOW));
+        Text.Builder offline = Text.of(TextColors.GOLD, "Offline Players (" + (faction.Members.size() - onlinePlayers.size()) + "): ", TextColors.YELLOW).toBuilder();
+        mainLoop: for(int i = 0, j = 0; i < faction.Members.size(); i++){
+            for(Player p : onlinePlayers) {
+                if(p.getUniqueId().toString().equals(faction.Members.get(i).name)) {
+                    text.append(p.getDisplayNameData().displayName().get());
+                    if ((j += 1) != onlinePlayers.size()) {
+                        text.append(Text.of(", "));
+                    }
+                    continue mainLoop;
+                }
+            }
+            offline.append(Text.of(PlayerManager.getPlayerName(UUID.fromString(faction.Members.get(i).name)).get()));
+            if (i - j != faction.Members.size() - onlinePlayers.size() - 1) {
+                offline.append(Text.of(", "));
+            }
+        }
+        text.append(Text.of("\n", offline.build()));
+        source.sendMessage(text.build());
     }
 }
